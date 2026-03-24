7830933d4b4711223ea3eabb2c662749c858a92a
Sweta20000 <99673840+Sweta20000@users.noreply.github.com>
2026-03-24 21:32:33 +0530

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,8 @@
+// Variables for Sweta’s portfolio
+let name = "Sweta Singh";
+const hobbies = ["Gym", "Swimming"];
+let skills = ["HTML", "CSS", "Bootstrap", "JavaScript"];
+
+console.log("Welcome to " + name + "’s portfolio!");
+console.log("Skills: " + skills.join(", "));
+console.log("Hobbies: " + hobbies.join(", "));
