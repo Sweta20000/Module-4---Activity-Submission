07dc2e5d76e746d9f87797bec32554f8817dbad6
Sweta20000 <99673840+Sweta20000@users.noreply.github.com>
2026-03-24 22:35:11 +0530

    Add files via upload
    ACP_SUBMISSION

---
+++ ObjectDataStructureandArray_ACP_SUBMISSION.js
@@ -0,0 +1,12 @@
+let student = {
+  name: "Sweta",
+  age: 25,
+  hobby: "Swimming"
+};
+// Simple Array
+let skills = ["HTML", "CSS", "Bootstrap"];
+
+console.log(skills[0]); // HTML
+console.log(skills[2]); // Bootstrap
+console.log(student.name); // Sweta
+console.log(student.hobby); // Swimming
